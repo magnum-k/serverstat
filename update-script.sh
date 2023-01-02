4de570234738d18d4341b1e19b8e767d83d31d84
magnum-k <82119168+magnum-k@users.noreply.github.com>
2023-01-02 13:43:04 +0100

    Update update-script.sh

--- update-script.sh
+++ update-script.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-cd /path/to/repository
+
+# Load the config file
+source config.sh
+
+# Change to the repository directory
+cd $REPO_PATH
+
+# Pull the latest changes from the GitHub repository
 git pull
+
+# Install any new dependencies
 npm install
-pm2 restart index
+
+# Restart the Discord bot
+pm2 restart $BOT_SCRIPT
