686e8282aeb57cdda1c3b0c5fb83d1ecc21a9684
magnum-k <82119168+magnum-k@users.noreply.github.com>
2023-01-02 18:47:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,13 +12,18 @@ client.on("ready", async () => {
   const channel = client.channels.get(config.channelId);
   if (!channel) return;
 
-  // Send the initial message and save the message object
-  const message = await channel.send("Updating system usage...");
+  // Send the initial message and save the message ID
+  const topOutput = await updateMessage(client);
+  if (!topOutput) return;
+  const message = await channel.send("", { embed: topOutput });
   if (!message) return;
+  const messageId = message.id;
 
   // Update the message every 15 minutes
   setInterval(async () => {
-    updateMessage(channel, message);
+    const topOutput = await updateMessage(client);
+    if (!topOutput) return;
+    updateMessage(channel, messageId, topOutput);
   }, 15 * 60 * 1000);
 });
 
