efae130771e06ec043267cb4174d4a0b75bdc1d0
magnum-k <82119168+magnum-k@users.noreply.github.com>
2023-01-03 12:54:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const { token } = require('./config.json');
+const { updateMessage } = require("./system-usage");
 
 const { Client, GatewayIntentBits } = require('discord.js')
 const client = new Client({
@@ -10,7 +11,7 @@ const client = new Client({
     ]
 })
 // const client = new Client({ intents: [GatewayIntentBits.Guild] });
-
+/*
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -72,6 +73,36 @@ client.on("message", (msg) => {
   // if (msg.content === "Hello") {
   //   sendMessage("Hello!");
   // }
+});
+*/
+
+client.on("ready", async () => {
+const guild = client.guild.cache.get(config.guildId);
+if (!guild) return;
+const channel = guild.channels.cache.find(channel => channel.name === config.channelName);
+if (!channel) return;
+
+  // Send the initial message and save the message ID
+  const topOutput = await updateMessage(client);
+  if (!topOutput) return;
+  const message = await channel.send("", { embed: topOutput });
+  if (!message) return;
+  const messageId = message.id;
+
+  // Update the message every 15 minutes
+  setInterval(async () => {
+    const topOutput = await updateMessage(client);
+    if (!topOutput) return;
+    updateMessage(channel, message, topOutput);
+  }, 15 * 60 * 1000);
+
+client.on("message", async message => {
+  if (message.content === config.updateCommand) {
+    const topOutput = await updateMessage(client);
+    if (!topOutput) return;
+    updateMessage(channel, message, topOutput);
+    }
+  });
 });
 
 client.login(token);
