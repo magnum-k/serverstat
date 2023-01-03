74add4719a8ccfd84b5ba2205797aa92c4f86dec
magnum-k <82119168+magnum-k@users.noreply.github.com>
2023-01-03 11:06:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,33 +36,3 @@ client.on("message", (msg) => {
   };
   
 client.login(token);
-
-// client.on("ready", async () => {
-// const guild = client.guild.cache.get(config.guildId);
-// if (!guild) return;
-// const channel = guild.channels.cache.find(channel => channel.name === config.channelName);
-// if (!channel) return;
-/*
-  // Send the initial message and save the message ID
-  const topOutput = await updateMessage(client);
-  if (!topOutput) return;
-  const message = await channel.send("", { embed: topOutput });
-  if (!message) return;
-  const messageId = message.id;
-
-  // Update the message every 15 minutes
-  setInterval(async () => {
-    const topOutput = await updateMessage(client);
-    if (!topOutput) return;
-    updateMessage(channel, message, topOutput);
-  }, 15 * 60 * 1000);
-
-client.on("message", async message => {
-  if (message.content === config.updateCommand) {
-    const topOutput = await updateMessage(client);
-    if (!topOutput) return;
-    updateMessage(channel, message, topOutput);
-    }
-  });
-});
-*/
