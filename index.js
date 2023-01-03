daf3b09ab83f81594b9d0a28e43b5fd05fbcae01
magnum-k <82119168+magnum-k@users.noreply.github.com>
2023-01-03 19:44:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 
 const client = new Client({
     intents: [
-        GatewayIntentBits.Guilds, GatewayIntentsBits.MessageContent
+        GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent
         // ...
     ]
 })
