5e17b907728cb56e479d626c1257e1e90b513de5
magnumk <magnus.karrman@gmail.com>
2023-01-04 07:46:45 +0100

    upfsyr

--- index.js
+++ index.js
@@ -21,28 +21,43 @@ client.on('messageCreate', (message) => {
 //    message.channel.send({ embeds: [embed] });
 
  if (message.content === '!update') {
-   exec('top -bn1', (error, stdout, stderr) => {
+  exec('ps -Ao %cpu,%mem', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error}`);
+      return;
+    }
+
+  // Calculate the total CPU usage in percent
+    const cpuUsageLines = stdout.split('\n').slice(1);
+    let totalCpuUsage = 0;
+    cpuUsageLines.forEach((line) => {
+      totalCpuUsage += parseFloat(line.split(' ')[0]);
+    });
+    totalCpuUsage = totalCpuUsage.toFixed(2);
+
+    exec('free -m', (error, stdout, stderr) => {
       if (error) {
         console.error(`Error: ${error}`);
         return;
       }
 
-      const lines = stdout.split('\n');
-      const totalMemoryUsage = parseInt(lines[0]) / 1024;
-      const totalCpuUsage = (parseInt(lines[1]) / numCpus) * 100;
-      const top5Processes = getTop5Processes(lines);
+            // Calculate the total memory usage in percent
+      const memoryUsageLines = stdout.split('\n');
+      const totalMemory = parseInt(memoryUsageLines[1].split(' ')[1]);
+      const usedMemory = parseInt(memoryUsageLines[2].split(' ')[2]);
+      const totalMemoryUsage = ((usedMemory / totalMemory) * 100).toFixed(2);
 
       const embed = new EmbedBuilder()
         .setTitle('System Usage')
         .addFields(
-        { name: 'Total CPU Usage', value: `${totalCpuUsage}%`, inline: true },
-        { name: 'Total Memory Usage', value: `${totalMemoryUsage}MB`, inline: true },
-        { name: 'Top 5 Processes', value: top5Processes, inline: false }
+          { name: 'Total CPU Usage', value: `${totalCpuUsage}%`, inline: true },
+          { name: 'Total Memory Usage', value: `${totalMemoryUsage}%`, inline: true }
         );
 
-    message.channel.send({ embeds: [embed] });
+      message.channel.send({ embeds: [embed] });
     });
- };
+  });
+}
 
 function getTop5Processes(lines) {
   const top5Lines = lines.slice(7, 12);
