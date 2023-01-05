61921fc42ef7ec6a1264e98c311844bb018e9291
magnum-k <82119168+magnum-k@users.noreply.github.com>
2023-01-05 08:17:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -79,9 +79,10 @@ exec('free -m', (error, stdout, stderr) => {
       message.channel.send({ embeds: [embed] });
     });
 
-function getTop5Processes(lines) {
-  const top5Lines = lines.slice(7, 12);
-  return top5Lines.join('\n');
-};
-    
+/*function getTop5Processes(lines) {
+ // const top5Lines = lines.slice(7, 12);
+  // return top5Lines.join('\n');
+ };
+*/
+      
 client.login(token);
