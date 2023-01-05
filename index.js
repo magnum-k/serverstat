a74740c910a5f12f01aa96b68513529134c0df1b
magnumk <magnus.karrman@gmail.com>
2023-01-05 09:39:42 +0100

    timestamp

--- index.js
+++ index.js
@@ -79,11 +79,13 @@ exec('free -m', (error, stdout, stderr) => {
         .setTitle('System Usage')
         .addFields(
           { name: 'Total CPU Usage', value: `${totalCpuUsage}%`, inline: true },
-          { name: 'Total Memory Usage', value: `${totalMemoryUsage}% (Tot:${totalMemoryMb} Gb)`, inline: true },)
-          .addFields
-          ({ name: 'Top 5 processes', value: `${top5Processes}%`, inline: true })
-        );
-
+          { name: 'Total Memory Usage', value: `${totalMemoryUsage}% (Tot:${totalMemoryMb} Gb)`, inline: true },
+        )
+          .addFields(
+          { name: 'Top 5 processes', value: `${top5Processes}%`, inline: true }
+        )
+        .setTimestamp();
+        
       message.channel.send({ embeds: [embed] });
     });
   });
