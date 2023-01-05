697668557b82a40b759c265b1b51eb693d2009e3
magnumk <magnus.karrman@gmail.com>
2023-01-05 21:45:34 +0100

    update

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ exec('free -m', (error, stdout, stderr) => {
     console.error(`Error: ${stderr}`);
     return;
   }
-    const Uptime = stdout; 
+    const Uptime = stdout.split('\n'); 
 });
     
     
