c28890613e4f663ae5f9f9636f0658984c176cb5
magnumk <magnus.karrman@gmail.com>
2023-01-09 12:28:52 +0100

    trying `${Uptime}` again.-cvsdf

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ client.on('messageCreate', (message) => {
                         { name: 'Total Memory Usage', value: `${totalMemoryUsage}% | ${totalMemoryGb} Gb / ${usedMemoryGb} Gb`, inline: true },
                     )
                     .addFields(
-                        { name: 'Top 5 processes', value: Uptime, inline: false }
+                        { name: 'Top 5 processes', value: `${Uptime}`, inline: false }
                     )
                     .setTimestamp()
                     .setFooter({ text: 'Timestamp:', iconURL: 'https://www.dropbox.com/s/zis8oldi19r6thu/12G.png?dl=1' });
